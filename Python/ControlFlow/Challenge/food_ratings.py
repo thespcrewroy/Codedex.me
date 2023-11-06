5ba74dbad790cd46559e4d17927f05f9210e4208
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2023-11-06 13:44:52 -0500

    Create food_ratings.py

---
+++ Python/ControlFlow/Challenge/food_ratings.py
@@ -0,0 +1,12 @@
+rating = 0.00
+
+if rating > 4.5:
+  print('Extraordinary')
+elif rating > 4 and rating < 4.5:
+  print('Excellent')
+elif rating > 3 and rating <= 4:
+  print ('Good')
+elif rating > 2 and rating <= 3:
+  print ('Fair')
+else:
+  print('Poor')
