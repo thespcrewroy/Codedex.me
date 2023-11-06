b746263d8d078a14c52fd5703581636f61dddc18
Swapnil Roy <68826225+thespcrewroy@users.noreply.github.com>
2023-11-06 13:53:18 -0500

    Create snapple_facts.py

---
+++ Python/ControlFlow/Challenge/snapple_facts.py
@@ -0,0 +1,17 @@
+import random   # import random to be able to generate random integers
+
+fact = random.randint(0,5)  # generated a random integer from 0 to 5
+
+# randomly print out a fun fact (based off of the randomly generated number)
+if fact == 0:
+  print('Flamingos turn pink from eating shrimp.')
+elif fact == 1:
+  print("The only food that doesn't spoil is honey.")
+elif fact == 2:
+  print('Shrimp can only swim backwards.')
+elif fact == 3:
+  print("A taste bud's life span is about 10 days.")
+elif fact == 4:
+  print('It is impossible to sneeze while sleeping.')
+elif fact == 5:
+  print('It is illegal to sing off-key in North Carolina.')
